67edfe0cff81f380cb8480023e0b79027bb36379
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-06-30 18:53:27 -0700

    frontend

--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -16,26 +16,50 @@ function ProductScreen(props){
   </div>
 <div className="details-info">
  <ul>
-  <li>
-    <h4>{product.name}</h4>
-  </li>
-  <li>
-  {product.rating} Stars ({product.numReviews} Reviews)
-  </li>
-  <li>
-    <b>${product.price}</b>
-  </li>
-  <li>
-  Discription:
-  <div>
-  {product.discription}
-  </div>
-  </li>
+    <li>
+      <h4>{product.name}</h4>
+    </li>
+    <li>
+      {product.rating} Stars ({product.numReviews} Reviews)
+    </li>
+    <li>
+     Price: <b>${product.price}</b>
+    </li>
+    <li>
+     Discription:
+     <div>
+        {product.description}
+      </div>
+    </li>
  </ul>
 
 </div>
-  </div>
+<div className="details-action">
+  <ul>
+    <li>
+      Price: {product.price}
+    </li>
+<li>
+  Status: {product.status}
+</li>
+<li>
+  Qty: <select>
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      </select>
+</li>
+<li>
+  <button className="button primary">
+    Add to Cart
+  </button>
+</li>
+</ul>
+</div>
+</div>
   
-  </div>
+</div>
 }
 export default ProductScreen;
