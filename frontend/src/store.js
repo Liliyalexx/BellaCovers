07675dff3fbf4c565c4cd9a0a7559d226478e4c0
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-07-14 20:23:58 -0700

    product router

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -11,6 +11,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer, 
   cart: cartReducer
 })
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+const composeEnhancer =
+window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState,  composeEnhancer(applyMiddleware(thunk)));
 export default store;
