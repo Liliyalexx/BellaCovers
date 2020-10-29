58daaa932986136b66af5349a6b22e86606bd015
Liliyalexx <59544038+Liliyalexx@users.noreply.github.com>
2020-10-28 21:51:31 -0700

    connect to MongoDB and create sample users

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,39 @@
 import express from 'express';
-import data from './data';
-import dotenv from 'dotenv';
-import config from './config';
 import mongoose from 'mongoose';
+import data from './data';
+// import dotenv from 'dotenv';
+// import config from './config';
 import userRouter from './routes/userRouter';
 
-dotenv.config();
-
-const mongodbUrl = config.MONGODB_URL;
-mongoose.connect('mongodb://localhost/bellacovers', {
-
-useNewUrlParser:true,
-useUnifiedTopology: true,
-useCreateIndex: true
-});
-
-
 const app = express();
-app.use("/api/users", userRouter)
-app.get("/api/products/:id", (req, res) => {
-  const productId = req.params.id;
-  const product = data.products.find(x => x._id === productId);
-  if (product)
-    res.send(product);
-  else
-    res.status(404).send({ msg: "Product Not Found." })
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/bellacovers', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
 
-app.get("/api/products", (req, res) => {
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
+app.use('/api/users', userRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
